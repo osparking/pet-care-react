d5b16f5fb5a16084daade62428b713155f23453f
osparking <jbpark03@gmail.com>
2024-09-30 16:02:10 +0900

    add three model classes having same attributes

---
+++ src/main/java/com/bumsoap/petcare/model/Admin.java
@@ -0,0 +1,13 @@
+package com.bumsoap.petcare.model;
+
+public class Admin {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private String userType;
+    private boolean isUsable;
+}
