584e7b12759288d0e49a8dea7f8d6b5b00d546b0
osparking <jbpark03@gmail.com>
2024-10-02 15:31:46 +0900

    make classes add Vet object into DB table

--- src/main/java/com/bumsoap/petcare/controller/ControllerVet.java
+++ src/main/java/com/bumsoap/petcare/controller/ControllerVet.java
@@ -1,4 +1,21 @@
 package com.bumsoap.petcare.controller;
 
+import com.bumsoap.petcare.model.Veterinarian;
+import com.bumsoap.petcare.service.ServiceVet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/veterinarians")
+@RequiredArgsConstructor
 public class ControllerVet {
+    private final ServiceVet serviceVet;
+
+    @PostMapping
+    public void add(@RequestBody Veterinarian veterinarian) {
+        serviceVet.add(veterinarian);
+    }
 }
--- src/main/java/com/bumsoap/petcare/service/ServiceVet.java
+++ src/main/java/com/bumsoap/petcare/service/ServiceVet.java
@@ -1,4 +1,16 @@
 package com.bumsoap.petcare.service;
 
+import com.bumsoap.petcare.model.Veterinarian;
+import com.bumsoap.petcare.repository.RepositoryVet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ServiceVet {
+    private final RepositoryVet repositoryVet;
+
+    public void add(Veterinarian veterinarian) {
+        repositoryVet.save(veterinarian);
+    }
 }
