8ae844c686a50c43aaceabb9ccf372f5d57738c4
osparking <jbpark03@gmail.com>
2024-10-04 15:25:34 +0900

    complete setting common attributes for User

--- src/main/java/com/bumsoap/petcare/service/user/UserAttributesMapper.java
+++ src/main/java/com/bumsoap/petcare/service/user/UserAttributesMapper.java
@@ -6,5 +6,13 @@ import com.bumsoap.petcare.request.RegistrationRequest;
 public class UserAttributesMapper {
     public void setCommonAttributes(RegistrationRequest source,
                                     User target) {
+        target.setFirstName(source.getFirstName());
+        target.setLastName(source.getLastName());
+        target.setGender(source.getGender());
+        target.setPhoneNumber(source.getPhoneNumber());
+        target.setEmail(source.getEmail());
+        target.setPassword(source.getPassword());
+        target.setEnabled(source.isEnabled());
+        target.setUserType(source.getUserType());
     }
 }
