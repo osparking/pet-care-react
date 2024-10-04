4973f2af77c408d51d00d7b403162f4f280d395a
osparking <jbpark03@gmail.com>
2024-10-04 12:32:27 +0900

    finish defining createVet' method

--- src/main/java/com/bumsoap/petcare/factory/FactoryVet.java
+++ src/main/java/com/bumsoap/petcare/factory/FactoryVet.java
@@ -1,10 +1,23 @@
 package com.bumsoap.petcare.factory;
 
 import com.bumsoap.petcare.model.User;
+import com.bumsoap.petcare.model.Veterinarian;
+import com.bumsoap.petcare.repository.RepositoryVet;
 import com.bumsoap.petcare.request.RegistrationRequest;
+import com.bumsoap.petcare.service.user.UserAttributesMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class FactoryVet {
-    public User createVeterinarian(RegistrationRequest registrationRequest) {
-        return null;
+    private final RepositoryVet veterinarianRepository;
+    private final UserAttributesMapper userAttributesMapper;
+
+    public User createVeterinarian(RegistrationRequest request) {
+        Veterinarian veterinarian = new Veterinarian();
+        userAttributesMapper.setCommonAttributes(request, veterinarian);
+        veterinarian.setSpecialization(request.getSpecialization());
+        return veterinarianRepository.save(veterinarian);
     }
 }
