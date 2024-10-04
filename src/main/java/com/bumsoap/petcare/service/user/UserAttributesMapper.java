9c6fd83938a6e510d394075302b689e42296ce56
osparking <jbpark03@gmail.com>
2024-10-04 12:29:57 +0900

    make UserAttributesMapper set method skeleton

---
+++ src/main/java/com/bumsoap/petcare/service/user/UserAttributesMapper.java
@@ -0,0 +1,10 @@
+package com.bumsoap.petcare.service.user;
+
+import com.bumsoap.petcare.model.Veterinarian;
+import com.bumsoap.petcare.request.RegistrationRequest;
+
+public class UserAttributesMapper {
+    public void setCommonAttributes(RegistrationRequest request,
+                                    Veterinarian veterinarian) {
+    }
+}
