abe9afcc827bb461595103d1de3a59a055c0c6d7
osparking <jbpark03@gmail.com>
2024-10-04 18:37:05 +0900

    implement create*() methods for 2 classes

--- src/main/java/com/bumsoap/petcare/factory/FactoryAdmin.java
+++ src/main/java/com/bumsoap/petcare/factory/FactoryAdmin.java
@@ -1,12 +1,23 @@
 package com.bumsoap.petcare.factory;
 
+import com.bumsoap.petcare.model.Admin;
 import com.bumsoap.petcare.model.User;
+import com.bumsoap.petcare.repository.RepositoryAdmin;
+import com.bumsoap.petcare.repository.RepositoryVet;
 import com.bumsoap.petcare.request.RegistrationRequest;
+import com.bumsoap.petcare.service.user.UserAttributesMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class FactoryAdmin {
-    public User createAdmin(RegistrationRequest registrationRequest) {
-        return null;
+    private final RepositoryAdmin repositoryAdmin;
+    private final UserAttributesMapper userAttributesMapper;
+
+    public User createAdmin(RegistrationRequest request) {
+        Admin admin = new Admin();
+        userAttributesMapper.setCommonAttributes(request, admin);
+        return repositoryAdmin.save(admin);
     }
 }
--- src/main/java/com/bumsoap/petcare/factory/FactoryPatient.java
+++ src/main/java/com/bumsoap/petcare/factory/FactoryPatient.java
@@ -1,12 +1,22 @@
 package com.bumsoap.petcare.factory;
 
+import com.bumsoap.petcare.model.Patient;
 import com.bumsoap.petcare.model.User;
+import com.bumsoap.petcare.repository.RepositoryPatient;
 import com.bumsoap.petcare.request.RegistrationRequest;
+import com.bumsoap.petcare.service.user.UserAttributesMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class FactoryPatient {
-    public User createPatient(RegistrationRequest registrationRequest) {
-        return null;
+    private final RepositoryPatient repositoryPatient;
+    private final UserAttributesMapper userAttributesMapper;
+
+    public User createPatient(RegistrationRequest request) {
+        Patient patient = new Patient();
+        userAttributesMapper.setCommonAttributes(request, patient);
+        return repositoryPatient.save(patient);
     }
 }
