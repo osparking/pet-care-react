8c8724ff061a00168d8747e9f263f8a37f9acc04
osparking <jbpark03@gmail.com>
2024-10-04 15:35:07 +0900

    finish UserAlreadyExistsException constructor

--- src/main/java/com/bumsoap/petcare/exception/UserAlreadyExistsException.java
+++ src/main/java/com/bumsoap/petcare/exception/UserAlreadyExistsException.java
@@ -1,6 +1,7 @@
 package com.bumsoap.petcare.exception;
 
 public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String s) {
+    public UserAlreadyExistsException(String message) {
+        super(message);
     }
 }
