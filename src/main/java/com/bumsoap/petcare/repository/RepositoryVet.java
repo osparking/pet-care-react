2b5191a4658845d0c498b0aeae2e52204ab63c3a
osparking <jbpark03@gmail.com>
2024-10-04 12:27:37 +0900

    add missing extends clause to RepoVet interface

--- src/main/java/com/bumsoap/petcare/repository/RepositoryVet.java
+++ src/main/java/com/bumsoap/petcare/repository/RepositoryVet.java
@@ -1,4 +1,7 @@
 package com.bumsoap.petcare.repository;
 
-public interface RepositoryVet {
+import com.bumsoap.petcare.model.Veterinarian;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositoryVet extends JpaRepository<Veterinarian, Long> {
 }
