13837cbab600fd42162cf62e1000256ef4cb0a31
osparking <jbpark03@gmail.com>
2024-10-04 15:23:53 +0900

    change field name to enabled

--- src/main/java/com/bumsoap/petcare/model/User.java
+++ src/main/java/com/bumsoap/petcare/model/User.java
@@ -23,7 +23,7 @@ public class User {
     private String email;
     private String password;
     private String userType;
-    private Boolean isUsable;
+    private Boolean enabled;
     @Transient
     private String specialization;
 }
