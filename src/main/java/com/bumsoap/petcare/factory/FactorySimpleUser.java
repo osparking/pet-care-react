22773575e066fde16719564cef2d562fbbe62d46
osparking <jbpark03@gmail.com>
2024-10-04 12:10:57 +0900

    complete user creation method for a simple user

--- src/main/java/com/bumsoap/petcare/factory/FactorySimpleUser.java
+++ src/main/java/com/bumsoap/petcare/factory/FactorySimpleUser.java
@@ -1,11 +1,34 @@
 package com.bumsoap.petcare.factory;
 
+import com.bumsoap.petcare.exception.UserAlreadyExistsException;
 import com.bumsoap.petcare.model.User;
+import com.bumsoap.petcare.repository.RepositoryUser;
 import com.bumsoap.petcare.request.RegistrationRequest;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class FactorySimpleUser implements FactoryUser{
+    private final RepositoryUser userRepository;
+    private final FactoryVet veterinarianFactory;
+    private final FactoryPatient patientFactory;
+    private final FactoryAdmin adminFactory;
+
     @Override
     public User createUser(RegistrationRequest registrationRequest) {
-        return null;
+        if(userRepository.existsByEmail(registrationRequest.getEmail())) {
+            throw new UserAlreadyExistsException("오류 - 등록된 이메일 : "
+                    + registrationRequest.getEmail());
+        }
+        switch (registrationRequest.getUserType()) {
+            case "VET" ->{return veterinarianFactory.createVeterinarian(registrationRequest);
+            }
+            case "PATIENT" -> { return  patientFactory.createPatient(registrationRequest);
+            }
+            case "ADMIN" -> {return adminFactory.createAdmin(registrationRequest);
+            }
+            default -> {
+                return null;
+            }
+        }
     }
 }
