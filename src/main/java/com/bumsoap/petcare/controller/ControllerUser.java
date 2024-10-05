5040b65a4c30a6f7548b6968390af5e4f49c9567
osparking <jbpark03@gmail.com>
2024-10-05 20:47:18 +0900

    use ApiResponse as add() response body - cherry

--- src/main/java/com/bumsoap/petcare/controller/ControllerUser.java
+++ src/main/java/com/bumsoap/petcare/controller/ControllerUser.java
@@ -2,9 +2,14 @@ package com.bumsoap.petcare.controller;
 
 import com.bumsoap.petcare.model.User;
 import com.bumsoap.petcare.request.RegistrationRequest;
+import com.bumsoap.petcare.response.ApiResponse;
 import com.bumsoap.petcare.service.user.ServiceUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
@@ -13,7 +18,10 @@ public class ControllerUser {
     private final ServiceUser serviceUser;
 
     @PostMapping
-    public User add(@RequestBody RegistrationRequest request) {
-        return serviceUser.add(request);
+    public ResponseEntity<ApiResponse> add(@RequestBody RegistrationRequest request) {
+        try {
+            User userSaved = serviceUser.add(request);
+        } catch (Exception e) {}
+        return null;
     }
 }
