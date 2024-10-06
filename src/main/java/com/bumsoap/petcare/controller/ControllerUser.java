8939f171ae443f39ef3773ec005b164926574e48
osparking <jbpark03@gmail.com>
2024-10-06 16:08:08 +0900

    define URL string mapping class

--- src/main/java/com/bumsoap/petcare/controller/ControllerUser.java
+++ src/main/java/com/bumsoap/petcare/controller/ControllerUser.java
@@ -8,6 +8,7 @@ import com.bumsoap.petcare.request.RegistrationRequest;
 import com.bumsoap.petcare.response.ApiResponse;
 import com.bumsoap.petcare.service.user.ServiceUser;
 import com.bumsoap.petcare.utils.FeedbackMessage;
+import com.bumsoap.petcare.utils.UrlMapping;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,7 @@ import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping(UrlMapping.USERS)
 @RequiredArgsConstructor
 public class ControllerUser {
     private final ServiceUser serviceUser;
---
+++ src/main/java/com/bumsoap/petcare/utils/UrlMapping.java
@@ -0,0 +1,6 @@
+package com.bumsoap.petcare.utils;
+
+public class UrlMapping {
+    public static final String API = "/api";
+    public static final String USERS = API + "/users";
+}
