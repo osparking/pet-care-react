db0c3e0a7f32d664173509b56685d14f5dff2c5c
osparking <jbpark03@gmail.com>
2024-10-07 12:29:17 +0900

    add /v1 to API url string constant

--- src/main/java/com/bumsoap/petcare/utils/UrlMapping.java
+++ src/main/java/com/bumsoap/petcare/utils/UrlMapping.java
@@ -1,7 +1,7 @@
 package com.bumsoap.petcare.utils;
 
 public class UrlMapping {
-    public static final String API = "/api";
+    public static final String API = "/api/v1";
     public static final String USERS = API + "/users";
     public static final String REGISTER_USER = "/register";
     public static final String UPDATE_USER = "/update/{userId}";
