dd516251ecb15d8fdf6a6dfe33ffbd6d54c56af8
osparking <jbpark03@gmail.com>
2024-10-09 10:55:05 +0900

    add CRUD methods in appointment service interface

--- src/main/java/com/bumsoap/petcare/service/appointment/IServiceAppointment.java
+++ src/main/java/com/bumsoap/petcare/service/appointment/IServiceAppointment.java
@@ -1,4 +1,16 @@
 package com.bumsoap.petcare.service.appointment;
 
+import com.bumsoap.petcare.model.Appointment;
+
+import java.util.List;
+
 public interface IServiceAppointment {
+    Appointment createAppointment(Appointment appointment,
+                                  Long senderId, Long recipientId);
+    List<Appointment> getAllAppointments();
+    Appointment updateAppointment(Long id, Appointment appointment);
+    void deleteAppointment(Long id);
+
+    Appointment getAppointmentById(Long appointmentId);
+    Appointment getByAppointmentNo(String appointmentNo);
 }
