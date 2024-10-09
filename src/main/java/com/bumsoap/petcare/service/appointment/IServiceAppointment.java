298a14e4d69488d7db49a574cbc00cfd35b520bb
osparking <jbpark03@gmail.com>
2024-10-09 10:28:59 +0900

    interface and class for service - appointment

---
+++ src/main/java/com/bumsoap/petcare/service/appointment/IServiceAppointment.java
@@ -0,0 +1,4 @@
+package com.bumsoap.petcare.service.appointment;
+
+public interface IServiceAppointment {
+}
---
+++ src/main/java/com/bumsoap/petcare/service/appointment/ServiceAppointment.java
@@ -0,0 +1,4 @@
+package com.bumsoap.petcare.service.appointment;
+
+public class ServiceAppointment {
+}
