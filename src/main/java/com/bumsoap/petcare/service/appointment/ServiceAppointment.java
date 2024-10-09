8873e671f15b45248478edd18335984d9428516c
osparking <jbpark03@gmail.com>
2024-10-09 11:14:07 +0900

    appointment service class implements interface

--- src/main/java/com/bumsoap/petcare/service/appointment/ServiceAppointment.java
+++ src/main/java/com/bumsoap/petcare/service/appointment/ServiceAppointment.java
@@ -1,4 +1,45 @@
 package com.bumsoap.petcare.service.appointment;
 
-public class ServiceAppointment {
+import com.bumsoap.petcare.model.Appointment;
+import com.bumsoap.petcare.repository.RepositoryAppointment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ServiceAppointment implements  IServiceAppointment{
+
+    private final RepositoryAppointment repositoryAppointment;
+    
+    @Override
+    public Appointment createAppointment(Appointment appointment, Long senderId, Long recipientId) {
+        return null;
+    }
+
+    @Override
+    public List<Appointment> getAllAppointments() {
+        return List.of();
+    }
+
+    @Override
+    public Appointment updateAppointment(Long id, Appointment appointment) {
+        return null;
+    }
+
+    @Override
+    public void deleteAppointment(Long id) {
+
+    }
+
+    @Override
+    public Appointment getAppointmentById(Long appointmentId) {
+        return null;
+    }
+
+    @Override
+    public Appointment getByAppointmentNo(String appointmentNo) {
+        return null;
+    }
 }
