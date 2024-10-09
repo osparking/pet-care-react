b8976cbc870383252eafc0a58b32c484c079ed60
osparking <jbpark03@gmail.com>
2024-10-09 12:05:48 +0900

    pet doctor/vet appointment status

---
+++ src/main/java/com/bumsoap/petcare/utils/StatusAppointment.java
@@ -0,0 +1,12 @@
+package com.bumsoap.petcare.utils;
+
+public enum StatusAppointment {
+    CANCELLED,
+    ON_GOING,
+    UP_COMING,
+    APPROVED,
+    REJECTED,
+    APPROVE_WAIT,
+    PENDING,
+    COMPLETED,
+}
