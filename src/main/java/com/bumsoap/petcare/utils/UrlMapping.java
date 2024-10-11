2d99daf9135dabc3e11fdf70ec42eb532d7b458b
osparking <jbpark03@gmail.com>
2024-10-11 15:13:37 +0900

    add some syntactic sugar on UrlMapping class

--- src/main/java/com/bumsoap/petcare/utils/UrlMapping.java
+++ src/main/java/com/bumsoap/petcare/utils/UrlMapping.java
@@ -8,13 +8,17 @@ public class UrlMapping {
     public static final String USER_BY_ID = "/user/{userId}";
     public static final String DELETE_USER = "/delete/{userId}";
     public static final String GET_ALL = "/get_all";
+
+    /*========================== Start Appointment API =================================*/
     public static final String APPOINTMENT = API + "/appointments";
+    /**
+     * 아래 URL 형태가 최신 유행
+     */
+    public static final String UPDATE_APPOINTMENT_BY_ID = "/{id}/update";
     public static final String CREATE = "/create";
     public static final String APPOINTMENT_BY_ID = "/{id}/get_id";
     public static final String DELETE_BY_ID = "/{id}/delete";
     public static final String APPOINTMENT_BY_NO = "/{no}/get_no";
-    /**
-     * 이것이 최신 유행 URL 형태
-     */
-    public static final String UPDATE_APPOINTMENT_BY_ID = "/{id}/update";
+    /*========================= End Appointment API ================================*/
+
 }
