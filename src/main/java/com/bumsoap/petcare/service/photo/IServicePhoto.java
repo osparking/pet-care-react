e651ca4bfb8f06684bf664066c65b48063e99883
osparking <jbpark03@gmail.com>
2024-10-13 15:30:13 +0900

    define save, find, delete methods

--- src/main/java/com/bumsoap/petcare/service/photo/IServicePhoto.java
+++ src/main/java/com/bumsoap/petcare/service/photo/IServicePhoto.java
@@ -1,4 +1,12 @@
 package com.bumsoap.petcare.service.photo;
 
+import com.bumsoap.petcare.model.Photo;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
 public interface IServicePhoto {
+    Photo save(Long userId, MultipartFile photo);
+    Optional<Photo> findById(Long id);
+    void deleteById(Long id);
 }
