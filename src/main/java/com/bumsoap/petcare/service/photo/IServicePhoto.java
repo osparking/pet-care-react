ded2fac52d927f654ad03d06bc33f0f3df3fc40b
osparking <jbpark03@gmail.com>
2024-10-13 19:34:17 +0900

    implement save photo service method

--- src/main/java/com/bumsoap/petcare/service/photo/IServicePhoto.java
+++ src/main/java/com/bumsoap/petcare/service/photo/IServicePhoto.java
@@ -3,10 +3,12 @@ package com.bumsoap.petcare.service.photo;
 import com.bumsoap.petcare.model.Photo;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Optional;
 
 public interface IServicePhoto {
-    Photo save(Long userId, MultipartFile photo);
+    Photo save(Long userId, MultipartFile photo) throws IOException, SQLException;
     Optional<Photo> findById(Long id);
     void deleteById(Long id);
     Photo update(Long id, byte[] imageData);
--- src/main/java/com/bumsoap/petcare/service/photo/ServicePhoto.java
+++ src/main/java/com/bumsoap/petcare/service/photo/ServicePhoto.java
@@ -1,13 +1,18 @@
 package com.bumsoap.petcare.service.photo;
 
+import com.bumsoap.petcare.exception.ResourceNotFoundException;
 import com.bumsoap.petcare.model.Photo;
 import com.bumsoap.petcare.model.User;
 import com.bumsoap.petcare.repository.RepositoryPhoto;
 import com.bumsoap.petcare.repository.RepositoryUser;
+import com.bumsoap.petcare.utils.FeedbackMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Optional;
 
 @Service
@@ -17,13 +22,23 @@ public class ServicePhoto implements IServicePhoto  {
     private final RepositoryPhoto repositoryPhoto; // Assuming RepositoryPhoto is a Spring Data JPA Repository for Photo entity
     private final RepositoryUser repositoryUser; // Assuming RepositoryUser is a Spring Data JPA
     @Override
-    public Photo save(Long userId, MultipartFile file) {
-        Optional<User> theUser = repositoryUser.findById(userId);
+    public Photo save(Long userId, MultipartFile file)
+            throws IOException, SQLException {
         Photo photo = new Photo();
 
         if (file != null && !file.isEmpty()) {
+            photo.setImage(new SerialBlob(file.getBytes()));
+            photo.setFileType(file.getContentType());
         }
-        return null;
+        Photo savedPhoto = repositoryPhoto.save(photo);
+
+        Optional<User> theUser = repositoryUser.findById(userId);
+        theUser.ifPresentOrElse(user -> { user.setPhoto(savedPhoto);},
+                ()-> {
+            throw new ResourceNotFoundException(FeedbackMessage.NOT_FOUND);
+        });
+        repositoryUser.save(theUser.get());
+        return savedPhoto;
     }
 
     @Override
