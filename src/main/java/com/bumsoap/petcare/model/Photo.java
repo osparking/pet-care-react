f026d2799d949270f0d74c33f1c82208251ee16e
osparking <jbpark03@gmail.com>
2024-10-13 12:13:50 +0900

    add some annotations onto Photo class

--- src/main/java/com/bumsoap/petcare/model/Photo.java
+++ src/main/java/com/bumsoap/petcare/model/Photo.java
@@ -1,9 +1,21 @@
 package com.bumsoap.petcare.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.sql.Blob;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fileType;
+    @Lob
     private Blob image;
 }
