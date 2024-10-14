7e1806abb3aaf95ceb2b3c8dcaef537f545bcc8c
osparking <jbpark03@gmail.com>
2024-10-14 12:12:31 +0900

    define and use photo Url mapping

--- src/main/java/com/bumsoap/petcare/controller/ControllerPhoto.java
+++ src/main/java/com/bumsoap/petcare/controller/ControllerPhoto.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 @RestController
-@RequestMapping("/photos")
+@RequestMapping(UrlMapping.PHOTO)
 @RequiredArgsConstructor
 public class ControllerPhoto {
 
--- src/main/java/com/bumsoap/petcare/utils/UrlMapping.java
+++ src/main/java/com/bumsoap/petcare/utils/UrlMapping.java
@@ -16,15 +16,19 @@ public class UrlMapping {
     public static final String UPDATE_BY_ID = "/{id}/update";
     public static final String GET_BY_ID = "/{id}/get_id";
     public static final String DELETE_BY_ID = "/{id}/delete";
+    public static final String CREATE = "/create";
     /*========================== End Common API =================================*/
 
     /*========================== Start Appointment API =================================*/
     public static final String APPOINTMENT = API + "/appointments";
-    public static final String CREATE = "/create";
     public static final String APPOINTMENT_BY_NO = "/{no}/get_no";
     public static final String PETS = API + "/pets";
     public static final String ADD_PETS_FOR_APPOINTMENT = "/for_appointment";
-    public static final String UPLOAD = "/upload";
     /*========================= End Appointment API ================================*/
+
+    /*============================ Start Photo API ===================================*/
+    public static final String PHOTO = API + "/photos";
+    public static final String UPLOAD = "/upload";
+    /*============================ End Photo API ===================================*/
 
 }
