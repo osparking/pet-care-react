3ea457479bbe6b5064be5915d224155f6a557038
osparking <jbpark03@gmail.com>
2024-10-15 12:18:02 +0900

    patient's doctor review class skeleton

---
+++ src/main/java/com/bumsoap/petcare/model/Review.java
@@ -0,0 +1,9 @@
+package com.bumsoap.petcare.model;
+
+public class Review {
+    private Long id;
+    private String comment;
+    private int stars;
+    private User veterinarian;
+    private User patient;
+}
