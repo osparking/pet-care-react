23c6e24427721c84f9a6ded54d62681a0c04293f
osparking <jbpark03@gmail.com>
2024-10-16 08:47:57 +0900

    JPQL for find review by userId

--- src/main/java/com/bumsoap/petcare/repository/IRepositoryReview.java
+++ src/main/java/com/bumsoap/petcare/repository/IRepositoryReview.java
@@ -2,10 +2,13 @@ package com.bumsoap.petcare.repository;
 
 import com.bumsoap.petcare.model.Review;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IRepositoryReview extends JpaRepository<Review, Long> {
-    Page<Review> findAllByUserId(Long userId, PageRequest request);
+    @Query("SELECT r FROM Review r " +
+            "WHERE r.veterinarian.id = :userId OR r.patient.id = :userId")
+    Page<Review> findAllByUserId(@Param("userId") Long userId, Pageable pageable);
 }
