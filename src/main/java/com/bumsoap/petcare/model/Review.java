61760bb13bfba695fb0cff727640348381a8a58a
osparking <jbpark03@gmail.com>
2024-10-15 12:27:36 +0900

    many reviews apply to a vet' and came from a pat'

--- src/main/java/com/bumsoap/petcare/model/Review.java
+++ src/main/java/com/bumsoap/petcare/model/Review.java
@@ -1,9 +1,18 @@
 package com.bumsoap.petcare.model;
 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 public class Review {
     private Long id;
     private String comment;
     private int stars;
+
+    @ManyToOne
+    @JoinColumn(name = "vet_id")
     private User veterinarian;
+
+    @ManyToOne
+    @JoinColumn(name = "pat_id")
     private User patient;
 }
