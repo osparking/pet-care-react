0463f70b36cd46e158084c38afd8cb09a20444c1
osparking <jbpark03@gmail.com>
2024-10-16 08:30:10 +0900

    service class and get review by userId method

--- src/main/java/com/bumsoap/petcare/service/review/ServiceReview.java
+++ src/main/java/com/bumsoap/petcare/service/review/ServiceReview.java
@@ -1,4 +1,36 @@
 package com.bumsoap.petcare.service.review;
 
+import com.bumsoap.petcare.model.Review;
+import com.bumsoap.petcare.repository.IRepositoryReview;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ServiceReview implements IServiceReview {
+
+    private final IRepositoryReview repositoryReview;
+
+    @Override
+    public void saveReview(Long patId, Long vetId, Review review) {
+
+    }
+
+    @Override
+    public double getAverageRatingForVet(Long vetId) {
+        return 0;
+    }
+
+    @Override
+    public void updateReview(Long patId, Review review) {
+
+    }
+
+    @Override
+    public Page<Review> getAllReviewsByUserId(Long userId, int page, int size) {
+        PageRequest request = PageRequest.of(page, size);
+        return repositoryReview.findAllByUserId(userId, request);
+    }
 }
