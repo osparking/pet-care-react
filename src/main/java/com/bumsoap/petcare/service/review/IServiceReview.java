f3a3ca345024357e3c6cee4653101e1d1a26bfe8
osparking <jbpark03@gmail.com>
2024-10-15 16:03:55 +0900

    add 3 more review related service methods

--- src/main/java/com/bumsoap/petcare/service/review/IServiceReview.java
+++ src/main/java/com/bumsoap/petcare/service/review/IServiceReview.java
@@ -1,7 +1,11 @@
 package com.bumsoap.petcare.service.review;
 
 import com.bumsoap.petcare.model.Review;
+import org.springframework.data.domain.Page;
 
 public interface IServiceReview {
     void saveReview(Long patId, Long vetId, Review review);
+    double getAverageRatingForVet(Long vetId);
+    void updateReview(Long patId, Review review);
+    Page<Review> getAllReviewsByUserId(Long userId, int page, int size);
 }
