0753529815c024a2d41cf082e5cc0bdb43de74f9
osparking <jbpark03@gmail.com>
2024-10-19 15:58:18 +0900

    add a few heavy fields - Dto version

--- src/main/java/com/bumsoap/petcare/dto/DtoUser.java
+++ src/main/java/com/bumsoap/petcare/dto/DtoUser.java
@@ -1,16 +1,27 @@
 package com.bumsoap.petcare.dto;
 
+import com.bumsoap.petcare.model.Appointment;
+import com.bumsoap.petcare.model.Review;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 public class DtoUser {
     private long id;
-    private String firstName;
     private String lastName;
+    private String firstName;
     private String gender;
     private String phoneNumber;
     private String email;
     private String userType;
     private Boolean enabled;
     private String specialization;
+    private LocalDate createdAt;
+    private List<DtoAppointment> appointments;
+    private List<DtoReview> reviews;
+    private long photoId;
+    private byte[] photo;
+    private double averageRating;
 }
