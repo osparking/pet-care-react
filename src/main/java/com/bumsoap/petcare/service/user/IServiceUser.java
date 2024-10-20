cb764d3e06de55e1a549ade6ee7691f9bb0d1de7
osparking <jbpark03@gmail.com>
2024-10-20 20:21:11 +0900

    pull getDtoUserById into interface

--- src/main/java/com/bumsoap/petcare/service/user/IServiceUser.java
+++ src/main/java/com/bumsoap/petcare/service/user/IServiceUser.java
@@ -5,6 +5,7 @@ import com.bumsoap.petcare.model.User;
 import com.bumsoap.petcare.request.RegistrationRequest;
 import com.bumsoap.petcare.request.UserUpdateRequest;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IServiceUser {
@@ -17,4 +18,6 @@ public interface IServiceUser {
     User findById(Long userId);
 
     List<DtoUser> getAllUsers();
+
+    DtoUser getDtoUserById(Long userId) throws SQLException;
 }
