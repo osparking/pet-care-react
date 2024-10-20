ee283819bce3fab7f18d4e784b6d38f613e8c46e
osparking <jbpark03@gmail.com>
2024-10-20 09:18:05 +0900

    imple' first half of getDtoUserById()

--- src/main/java/com/bumsoap/petcare/service/user/ServiceUser.java
+++ src/main/java/com/bumsoap/petcare/service/user/ServiceUser.java
@@ -8,10 +8,13 @@ import com.bumsoap.petcare.model.User;
 import com.bumsoap.petcare.repository.RepositoryUser;
 import com.bumsoap.petcare.request.RegistrationRequest;
 import com.bumsoap.petcare.request.UserUpdateRequest;
+import com.bumsoap.petcare.service.appointment.ServiceAppointment;
+import com.bumsoap.petcare.service.photo.IServicePhoto;
 import com.bumsoap.petcare.utils.FeedbackMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +24,8 @@ public class ServiceUser implements IServiceUser {
     private final FactoryUser userFactory;
     private final RepositoryUser repositoryUser;
     private final EntityConverter<User, DtoUser> entityConverter;
+    private final ServiceAppointment serviceAppointment;
+    private final IServicePhoto servicePhoto;
 
     @Override
     public User register(RegistrationRequest request) {
@@ -60,4 +65,38 @@ public class ServiceUser implements IServiceUser {
                         (user -> entityConverter.mapEntityToDto(user, DtoUser.class))
                 .collect(Collectors.toList());
     }
+
+    public DtoUser getDtoUserById(Long userId) throws SQLException {
+        // 1. Find user by id
+        User user = findById(userId); // Throw exception if user not found
+
+        // 2. Convert user to DtoUser
+        DtoUser dtoUser = entityConverter.mapEntityToDto(user, DtoUser.class);
+
+        // 3. Get appointments for user(as patient or veterinarian)
+        var dtoAppos = serviceAppointment.getAllDtoAppointmentsByUserId(userId);
+        dtoUser.setAppointments(dtoAppos);
+
+        // 4. Get photo image data for the user
+        if (user.getPhoto() != null) {
+            long photoId = user.getPhoto().getId();
+
+            dtoUser.setPhotoId(photoId);
+            dtoUser.setPhoto(servicePhoto.getImageData(photoId));
+        }
+
+        // 5. Get reviews for user(as patient or veterinarian)
+
+        return dtoUser;
+    }
 }
+
+
+
+
+
+
+
+
+
+
