165bbbe8330848310b7ed1fb6c17ca265888c6d3
osparking <jbpark03@gmail.com>
2024-10-21 14:33:19 +0900

    2 user DTO classes w/ reduced fields

---
+++ src/main/java/com/bumsoap/petcare/dto/DtoUserAppo.java
@@ -0,0 +1,15 @@
+package com.bumsoap.petcare.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class DtoUserAppo {
+    private String lastName;
+    private String firstName;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+}
---
+++ src/main/java/com/bumsoap/petcare/dto/DtoUserAppoVet.java
@@ -0,0 +1,8 @@
+package com.bumsoap.petcare.dto;
+
+import lombok.Data;
+
+@Data
+public class DtoUserAppoVet extends DtoUserAppo{
+    private String specialization;
+}
