32dee0beb75225bf727d4e8bafa730df43fac0a1
osparking <jbpark03@gmail.com>
2024-11-28 14:59:18 +0900

    map a vet object to a user object

--- src/main/java/com/bumsoap/petcare/service/vet/VetService.java
+++ src/main/java/com/bumsoap/petcare/service/vet/VetService.java
@@ -1,4 +1,28 @@
 package com.bumsoap.petcare.service.vet;
 
+import com.bumsoap.petcare.dto.DtoUser;
+import com.bumsoap.petcare.dto.EntityConverter;
+import com.bumsoap.petcare.model.Veterinarian;
+import com.bumsoap.petcare.repository.IRepositoryReview;
+import com.bumsoap.petcare.repository.RepositoryVet;
+import com.bumsoap.petcare.service.review.IServiceReview;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class VetService {
+    private final RepositoryVet repositoryVet;
+    private final EntityConverter<Veterinarian, DtoUser> entityConverter;
+    private final IServiceReview serviceReview;
+    private final IRepositoryReview repositoryReview;
+
+    private DtoUser mapVeterinarianToDtoUser(Veterinarian veterinarian) {
+        DtoUser dtoUser = entityConverter.mapEntityToDto(veterinarian, DtoUser.class);
+        double ratingAvg = serviceReview.getAverageRatingForVet(veterinarian.getId());
+        Long reviewerTotal = repositoryReview.countByVeterinarianId(veterinarian.getId());
+        dtoUser.setAverageRating(ratingAvg);
+        dtoUser.setTotalReviewers(reviewerTotal);
+        return dtoUser;
+    }
 }
