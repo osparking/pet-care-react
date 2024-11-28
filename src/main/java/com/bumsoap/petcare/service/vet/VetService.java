241aaf66cc4ca6a3f41b9ec8b517a0581873265f
osparking <jbpark03@gmail.com>
2024-11-28 15:12:21 +0900

    set vet' photo data onto user object

--- src/main/java/com/bumsoap/petcare/service/vet/VetService.java
+++ src/main/java/com/bumsoap/petcare/service/vet/VetService.java
@@ -5,9 +5,12 @@ import com.bumsoap.petcare.dto.EntityConverter;
 import com.bumsoap.petcare.model.Veterinarian;
 import com.bumsoap.petcare.repository.IRepositoryReview;
 import com.bumsoap.petcare.repository.RepositoryVet;
+import com.bumsoap.petcare.service.photo.IServicePhoto;
 import com.bumsoap.petcare.service.review.IServiceReview;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +19,7 @@ public class VetService {
     private final EntityConverter<Veterinarian, DtoUser> entityConverter;
     private final IServiceReview serviceReview;
     private final IRepositoryReview repositoryReview;
+    private final IServicePhoto servicePhoto;
 
     private DtoUser mapVeterinarianToDtoUser(Veterinarian veterinarian) {
         DtoUser dtoUser = entityConverter.mapEntityToDto(veterinarian, DtoUser.class);
@@ -23,6 +27,14 @@ public class VetService {
         Long reviewerTotal = repositoryReview.countByVeterinarianId(veterinarian.getId());
         dtoUser.setAverageRating(ratingAvg);
         dtoUser.setTotalReviewers(reviewerTotal);
+        if (veterinarian.getPhoto() != null) {
+            try {
+                byte[] photoBytes = servicePhoto.getImageData(veterinarian.getPhoto().getId());
+                dtoUser.setPhoto(photoBytes);
+            } catch (SQLException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
         return dtoUser;
     }
 }
