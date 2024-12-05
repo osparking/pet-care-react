0811fd8709724649aeed754ac74cf110d8eb22e0
osparking <jbpark03@gmail.com>
2024-12-05 13:44:11 +0900

    select appointments for a vet on a date

--- src/main/java/com/bumsoap/petcare/repository/RepositoryAppointment.java
+++ src/main/java/com/bumsoap/petcare/repository/RepositoryAppointment.java
@@ -1,10 +1,13 @@
 package com.bumsoap.petcare.repository;
 
 import com.bumsoap.petcare.model.Appointment;
+import com.bumsoap.petcare.model.User;
+import com.bumsoap.petcare.model.Veterinarian;
 import com.bumsoap.petcare.utils.StatusAppointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface RepositoryAppointment extends JpaRepository<Appointment, Long> {
@@ -16,4 +19,6 @@ public interface RepositoryAppointment extends JpaRepository<Appointment, Long> 
     @Query("SELECT a FROM Appointment a " +
             "WHERE a.veterinarian.id = :userId OR a.patient.id = :userId")
     List<Appointment> findAllByUserId(Long userId);
+
+    List<Appointment> findByVeterinarianAndDate(User vet, LocalDate date);
 }
