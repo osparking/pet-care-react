b79f26fb74f4f95df784c1489d5bd10571c88107
osparking <jbpark03@gmail.com>
2024-12-05 19:22:23 +0900

    service interface - find available vets for an appoint

--- src/main/java/com/bumsoap/petcare/service/vet/IServiceVet.java
+++ src/main/java/com/bumsoap/petcare/service/vet/IServiceVet.java
@@ -2,8 +2,13 @@ package com.bumsoap.petcare.service.vet;
 
 import com.bumsoap.petcare.dto.DtoUser;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public interface IServiceVet {
     List<DtoUser> getAllVetsWithDetails();
+
+    List<DtoUser> getAvailVetsForAppointment(String speciality,
+                                             LocalDate date, LocalTime time);
 }
