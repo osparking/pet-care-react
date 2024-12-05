5f01debf23500ee4779a240f448625bce2258f30
osparking <jbpark03@gmail.com>
2024-12-05 11:57:19 +0900

    checks if two schedules overlap

--- src/main/java/com/bumsoap/petcare/service/vet/ServiceVet.java
+++ src/main/java/com/bumsoap/petcare/service/vet/ServiceVet.java
@@ -2,6 +2,7 @@ package com.bumsoap.petcare.service.vet;
 
 import com.bumsoap.petcare.dto.DtoUser;
 import com.bumsoap.petcare.dto.EntityConverter;
+import com.bumsoap.petcare.model.Appointment;
 import com.bumsoap.petcare.model.Veterinarian;
 import com.bumsoap.petcare.repository.IRepositoryReview;
 import com.bumsoap.petcare.repository.RepositoryUser;
@@ -11,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
+import java.time.LocalTime;
 import java.util.List;
 
 @Service
@@ -28,6 +30,22 @@ public class ServiceVet implements IServiceVet {
         return veterinarians.stream()
                .map(this::mapVeterinarianToDtoUser)
                .toList();
+    }
+
+//        LocalTime exStart = existingAppo.getTime(); // 기존 시작: 오후 2시
+//        LocalTime exStop = exStart.plusHours(2); // 기존 종료: 오후 4시
+//        LocalTime exMarginStart = exStart.minusHours(1); // 기존 시작 앞 1시간 마진: 오후 1시
+//        LocalTime exMarginStop = exStop.plusHours(3); // 기존 종료 후 1시간 마진: 오후
+//        return !(tryEnd.compareTo(exMarginStart) <= 0 || tryStart.compareTo(exMarginStop) >= 0);
+    private boolean apposOverlap(Appointment existingAppointment,
+                                 LocalTime requestedStartTime,
+                                 LocalTime requestedEndTime) {
+        LocalTime existingStartTime = existingAppointment.getTime();
+        LocalTime existingEndTime = existingStartTime.plusHours(2);
+        LocalTime unavailableStartTime = existingStartTime.minusHours(1);
+        LocalTime unavailableEndTime = existingEndTime.plusMinutes(170);
+        return !requestedStartTime.isBefore(unavailableStartTime) &&
+                !requestedEndTime.isAfter(unavailableEndTime);
     }
 
     private DtoUser mapVeterinarianToDtoUser(Veterinarian veterinarian) {
