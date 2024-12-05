02e5fac741ffb7de6ddf69423d48ae829786d692
osparking <jbpark03@gmail.com>
2024-12-05 17:52:35 +0900

    custom jpa method for vet list of a special

--- src/main/java/com/bumsoap/petcare/repository/RepositoryVet.java
+++ src/main/java/com/bumsoap/petcare/repository/RepositoryVet.java
@@ -3,5 +3,8 @@ package com.bumsoap.petcare.repository;
 import com.bumsoap.petcare.model.Veterinarian;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RepositoryVet extends JpaRepository<Veterinarian, Long> {
+    List<Veterinarian> findVeterinarianBySpecialization(String speciality);
 }
