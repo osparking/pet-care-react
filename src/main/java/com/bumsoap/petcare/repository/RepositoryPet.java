74790dd05a80f3b2fada0195dd0a84abf236bef6
osparking <jbpark03@gmail.com>
2024-12-12 15:39:48 +0900

    specify custom query for pet reposi methods

--- src/main/java/com/bumsoap/petcare/repository/RepositoryPet.java
+++ src/main/java/com/bumsoap/petcare/repository/RepositoryPet.java
@@ -2,13 +2,17 @@ package com.bumsoap.petcare.repository;
 
 import com.bumsoap.petcare.model.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface RepositoryPet extends JpaRepository<Pet, Long> {
+    @Query("SELECT DISTINCT p.color FROM Pet p")
     List<Pet> getDistinctColors(); // Define JPA Repository interface for Pet entity{
 
+    @Query("SELECT DISTINCT p.type FROM Pet p")
     List<Pet> getDistinctTypes();
 
+    @Query("SELECT DISTINCT p.breed FROM Pet p WHERE p.type = :type")
     List<Pet> getDistinctBreedsByType(String type);
 }
