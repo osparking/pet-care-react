61be850bfa93549fa906d30d3dd10bd63394399d
osparking <jbpark03@gmail.com>
2024-12-26 09:02:56 +0900

    queries distinctive specialization of Vets

--- src/main/java/com/bumsoap/petcare/repository/RepositoryVet.java
+++ src/main/java/com/bumsoap/petcare/repository/RepositoryVet.java
@@ -2,10 +2,13 @@ package com.bumsoap.petcare.repository;
 
 import com.bumsoap.petcare.model.Veterinarian;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface RepositoryVet extends JpaRepository<Veterinarian, Long> {
     List<Veterinarian> findVeterinarianBySpecialization(String speciality);
     boolean existsBySpecialization(String speciality);
+    @Query("SELECT DISTINCT v.specialization FROM veterinarian v")
+    List<String> getDistinctSpecial();
 }
