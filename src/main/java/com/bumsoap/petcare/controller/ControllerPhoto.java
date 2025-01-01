c5c30b98c014fabbe00ea71aa98cef77dc8a9aa4
osparking <jbpark03@gmail.com>
2025-01-01 18:17:56 +0900

    assign crossorigin anno to Controller Photo

--- src/main/java/com/bumsoap/petcare/controller/ControllerPhoto.java
+++ src/main/java/com/bumsoap/petcare/controller/ControllerPhoto.java
@@ -22,6 +22,7 @@ import java.sql.SQLException;
 @RestController
 @RequestMapping(UrlMapping.PHOTO)
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:5173/")
 public class ControllerPhoto {
 
     private final IServicePhoto servicePhoto;
