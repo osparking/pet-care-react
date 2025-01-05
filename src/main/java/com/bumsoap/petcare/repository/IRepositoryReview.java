7478049bba7eac9e30d10ce9240405d578ba43b3
osparking <jbpark03@gmail.com>
2025-01-05 11:28:19 +0900

    read all reviews related to a user

--- src/main/java/com/bumsoap/petcare/repository/IRepositoryReview.java
+++ src/main/java/com/bumsoap/petcare/repository/IRepositoryReview.java
@@ -15,6 +15,10 @@ public interface IRepositoryReview extends JpaRepository<Review, Long> {
             "WHERE r.veterinarian.id = :userId OR r.patient.id = :userId")
     Page<Review> findAllByUserId(@Param("userId") Long userId, Pageable pageable);
 
+    @Query("SELECT r FROM Review r " +
+            "WHERE r.veterinarian.id = :userId OR r.patient.id = :userId")
+    List<Review> findAllByUserId(@Param("userId") Long userId);
+
     List<Review> findAllByVeterinarianId(Long vetId);
 
     Optional<Review> findByPatientIdAndVeterinarianId(Long patId, Long vetId);
