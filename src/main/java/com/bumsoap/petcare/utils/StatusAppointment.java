4376a30d8f10103eb41c234d62bd049e6b013817
osparking <jbpark03@gmail.com>
2025-01-07 14:46:41 +0900

    override toString of StatusAppointment enum class

--- src/main/java/com/bumsoap/petcare/utils/StatusAppointment.java
+++ src/main/java/com/bumsoap/petcare/utils/StatusAppointment.java
@@ -9,4 +9,29 @@ public enum StatusAppointment {
     APPROVE_WAIT,
     PENDING,
     COMPLETED,
+    ;
+    @Override
+    public String toString() {
+        switch (this) {
+            case CANCELLED:
+                return "취소됨";
+            case ON_GOING:
+                return "진료 중";
+            case UP_COMING:
+                return "임박함";
+            case APPROVED:
+                return "승인됨";
+            case REJECTED:
+                return "거부됨";
+            case APPROVE_WAIT:
+                return "승인대기";
+            case PENDING:
+                return "보류 중";
+            case COMPLETED:
+                return "완료됨";
+            default:
+                return null;
+        }
+    }
 }
+
