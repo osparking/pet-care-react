6677cff95138176d13f3d22ff228434b7dd744b6
osparking <jbpark03@gmail.com>
2025-01-09 18:33:47 +0900

    controller endpoint for appointment cancel

--- src/main/java/com/bumsoap/petcare/controller/ControllerAppointment.java
+++ src/main/java/com/bumsoap/petcare/controller/ControllerAppointment.java
@@ -136,4 +136,16 @@ public class ControllerAppointment {
         }
     }
 
+    @PutMapping(UrlMapping.APPO_CANCEL)
+    public ResponseEntity<ApiResponse> cancelAppointment(@PathVariable Long id) {
+        try {
+            Appointment appointment = serviceAppointment.cancelAppointment(id);
+            return ResponseEntity.ok(
+                    new ApiResponse(FeedbackMessage.RESOURCE_UPDATED, appointment));
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(NOT_ACCEPTABLE)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
 }
