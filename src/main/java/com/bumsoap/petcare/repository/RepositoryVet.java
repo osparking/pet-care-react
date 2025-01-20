991079779bf43192d4e4eb1f8d0ec9270476c772
osparking <jbpark03@gmail.com>
2025-01-20 11:31:47 +0900

    fix 'could not resolve root entity' error

--- src/main/java/com/bumsoap/petcare/repository/RepositoryVet.java
+++ src/main/java/com/bumsoap/petcare/repository/RepositoryVet.java
@@ -12,7 +12,7 @@ public interface RepositoryVet extends JpaRepository<Veterinarian, Long> {
     @Query("SELECT DISTINCT v.specialization FROM Veterinarian v")
     List<String> getDistinctSpecial();
 
-    @Query("select distinct v.specialization, count(*) from veterinarian v " +
+    @Query("select distinct v.specialization, count(*) from Veterinarian v " +
             "group by specialization order by count(*) desc")
     List<Object[]> countVetBySpecial();
 }
