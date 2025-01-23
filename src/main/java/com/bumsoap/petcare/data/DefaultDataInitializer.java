807bc6b4ec00c4395c9d17056bb91afddf526f34
osparking <jbpark03@gmail.com>
2025-01-23 21:37:40 +0900

    declare 3 reposi' references

--- src/main/java/com/bumsoap/petcare/data/DefaultDataInitializer.java
+++ src/main/java/com/bumsoap/petcare/data/DefaultDataInitializer.java
@@ -1,5 +1,8 @@
 package com.bumsoap.petcare.data;
 
+import com.bumsoap.petcare.repository.RepositoryPatient;
+import com.bumsoap.petcare.repository.RepositoryUser;
+import com.bumsoap.petcare.repository.RepositoryVet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
@@ -9,6 +12,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class DefaultDataInitializer
         implements ApplicationListener<ApplicationReadyEvent> {
+    private final RepositoryUser userRepository;
+    private final RepositoryVet veterinarianRepository;
+    private final RepositoryPatient patientRepository;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
