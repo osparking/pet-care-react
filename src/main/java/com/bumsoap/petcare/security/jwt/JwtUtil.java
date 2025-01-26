bce273756f384cb39acc1f2b7a6030a324200c64
osparking <jbpark03@gmail.com>
2025-01-26 19:00:03 +0900

    make JwtUtil a bean

--- src/main/java/com/bumsoap/petcare/security/jwt/JwtUtil.java
+++ src/main/java/com/bumsoap/petcare/security/jwt/JwtUtil.java
@@ -8,10 +8,12 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.List;
 
+@Component
 public class JwtUtil {
     private String jwtSecret;
     private int expirationMs;
