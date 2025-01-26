2e6269a4b3e54c790667660cea7a008b31c6da80
osparking <jbpark03@gmail.com>
2025-01-26 12:12:01 +0900

    object builder factory method and etc.

--- src/main/java/com/bumsoap/petcare/security/user/PcUserDetails.java
+++ src/main/java/com/bumsoap/petcare/security/user/PcUserDetails.java
@@ -1,13 +1,20 @@
 package com.bumsoap.petcare.security.user;
 
+import com.bumsoap.petcare.model.User;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PcUserDetails implements UserDetails {
     private Long id;
     private String email;
@@ -15,6 +22,15 @@ public class PcUserDetails implements UserDetails {
     private boolean enabled;
 
     private Collection<GrantedAuthority> authorities;
+
+    public static PcUserDetails buildUserDetails(User user) {
+        List<GrantedAuthority> authorities = user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+        return new PcUserDetails(user.getId(), user.getEmail(),
+                user.getPassword(), user.getEnabled(), authorities);
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
