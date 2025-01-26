14bda3fcdcef1158d6a7a3748d8a79d565c90bfe
osparking <jbpark03@gmail.com>
2025-01-26 14:33:52 +0900

    custom loadUserByUsername method

--- src/main/java/com/bumsoap/petcare/security/user/PcUserDetailsService.java
+++ src/main/java/com/bumsoap/petcare/security/user/PcUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.bumsoap.petcare.security.user;
 
+import com.bumsoap.petcare.model.User;
 import com.bumsoap.petcare.repository.RepositoryUser;
+import com.bumsoap.petcare.utils.FeedbackMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,8 +13,10 @@ public class PcUserDetailsService implements UserDetailsService {
     private final RepositoryUser repositoryUser;
 
     @Override
-    public UserDetails loadUserByUsername(String username)
+    public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
-        return null;
+        User user = repositoryUser.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException(FeedbackMessage.NOT_FOUND));
+        return PcUserDetails.buildUserDetails(user);
     }
 }
