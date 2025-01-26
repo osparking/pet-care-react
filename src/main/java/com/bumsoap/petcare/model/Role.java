ababb47a46d249f48df005cc0346c976648ad866
osparking <jbpark03@gmail.com>
2025-01-26 10:20:42 +0900

    annotate members, a constructor

--- src/main/java/com/bumsoap/petcare/model/Role.java
+++ src/main/java/com/bumsoap/petcare/model/Role.java
@@ -1,16 +1,24 @@
 package com.bumsoap.petcare.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
 
 @Entity
 @NoArgsConstructor
 @Data
 public class Role {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NaturalId
     private String name;
+    public Role(String name) {
+        this.name = name;
+    }
 
 }
