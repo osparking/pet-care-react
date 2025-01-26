2368df118cf416af21383a133fd6fbfaecbf1703
osparking <jbpark03@gmail.com>
2025-01-26 11:01:03 +0900

    specify join table for user and role tables

--- src/main/java/com/bumsoap/petcare/model/User.java
+++ src/main/java/com/bumsoap/petcare/model/User.java
@@ -37,6 +37,11 @@ public class User {
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = { CascadeType.DETACH,
             CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "user_roles",
+            joinColumns
+                    = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns
+                    = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private Collection<Role> roles = new HashSet<>();
 
     public String createdAtMonth() {
