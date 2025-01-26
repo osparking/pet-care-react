b07deb31d60c4b9902459322ee44764b9380e135
osparking <jbpark03@gmail.com>
2025-01-26 11:08:25 +0900

    define 5 private members of PcUserDetails

--- src/main/java/com/bumsoap/petcare/security/user/PcUserDetails.java
+++ src/main/java/com/bumsoap/petcare/security/user/PcUserDetails.java
@@ -1,4 +1,14 @@
 package com.bumsoap.petcare.security.user;
 
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
 public class PcUserDetails {
+    private Long id;
+    private String email;
+    private String password;
+    private boolean enabled;
+
+    private Collection<GrantedAuthority> authorities;
 }
