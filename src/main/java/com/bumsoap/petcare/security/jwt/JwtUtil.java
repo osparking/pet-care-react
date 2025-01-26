1b93e99c8e354eca6cae1e3144101e5aac907eb5
osparking <jbpark03@gmail.com>
2025-01-26 15:37:54 +0900

    returns a key to use in encoding

--- src/main/java/com/bumsoap/petcare/security/jwt/JwtUtil.java
+++ src/main/java/com/bumsoap/petcare/security/jwt/JwtUtil.java
@@ -1,9 +1,12 @@
 package com.bumsoap.petcare.security.jwt;
 
 import com.bumsoap.petcare.security.user.PcUserDetails;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
+import java.security.Key;
 import java.util.List;
 
 public class JwtUtil {
@@ -19,4 +22,7 @@ public class JwtUtil {
         return "";
     }
 
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
 }
