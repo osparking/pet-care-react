40afe2e5e573c9b49b838e31da05239b457fa3ee
osparking <jbpark03@gmail.com>
2025-01-26 19:42:22 +0900

    make subordinate of OncePerRequestFilter

--- src/main/java/com/bumsoap/petcare/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/bumsoap/petcare/security/jwt/AuthTokenFilter.java
@@ -1,12 +1,30 @@
 package com.bumsoap.petcare.security.jwt;
 
 import com.bumsoap.petcare.security.user.PcUserDetails;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthTokenFilter {
+public class AuthTokenFilter extends OncePerRequestFilter {
     private JwtUtil jwtUtil;
     PcUserDetails pcUserDetails;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+
+
+    }
 }
