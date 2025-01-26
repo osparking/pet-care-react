75e3c8bc79e08dd5c86e66e21a6706bb77068bf6
osparking <jbpark03@gmail.com>
2025-01-26 11:11:05 +0900

    make PcUD implements UserDetails inter'

--- src/main/java/com/bumsoap/petcare/security/user/PcUserDetails.java
+++ src/main/java/com/bumsoap/petcare/security/user/PcUserDetails.java
@@ -1,14 +1,33 @@
 package com.bumsoap.petcare.security.user;
 
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
-public class PcUserDetails {
+@Data
+public class PcUserDetails implements UserDetails {
     private Long id;
     private String email;
     private String password;
     private boolean enabled;
 
     private Collection<GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
 }
