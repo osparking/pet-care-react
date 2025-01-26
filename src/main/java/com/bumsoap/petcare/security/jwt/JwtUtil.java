03991cd3de050a883a5a3a610ea4c27051ad5fe3
osparking <jbpark03@gmail.com>
2025-01-26 15:26:12 +0900

    builds jwt token - the upper half

--- src/main/java/com/bumsoap/petcare/security/jwt/JwtUtil.java
+++ src/main/java/com/bumsoap/petcare/security/jwt/JwtUtil.java
@@ -1,7 +1,22 @@
 package com.bumsoap.petcare.security.jwt;
 
+import com.bumsoap.petcare.security.user.PcUserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+
 public class JwtUtil {
     private String jwtSecret;
     private int expirationMs;
+
+    public String generateTokenForUser(Authentication authentication) {
+        PcUserDetails userDetails = (PcUserDetails) authentication.getPrincipal();
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority).toList();
+
+        return "";
+    }
 
 }
