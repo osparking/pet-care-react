22f983a856863ae0eb876121d7ba32f9aa33c098
osparking <jbpark03@gmail.com>
2025-01-26 12:33:55 +0900

    override loadUserByUsername method

--- src/main/java/com/bumsoap/petcare/security/user/PcUserDetailsService.java
+++ src/main/java/com/bumsoap/petcare/security/user/PcUserDetailsService.java
@@ -1,4 +1,18 @@
 package com.bumsoap.petcare.security.user;
 
-public class PcUserDetailsService {
+import com.bumsoap.petcare.repository.RepositoryUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class PcUserDetailsService implements UserDetailsService {
+    private final RepositoryUser repositoryUser;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        return null;
+    }
 }
