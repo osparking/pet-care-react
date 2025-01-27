e9fcd4e43d3f5587d37c6874d4d3d5631af48985
osparking <jbpark03@gmail.com>
2025-01-27 09:55:29 +0900

    configures appl security - skeleton

---
+++ src/main/java/com/bumsoap/petcare/config/AppSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.bumsoap.petcare.config;
+
+import com.bumsoap.petcare.security.jwt.JwtAuthEntryPt;
+import com.bumsoap.petcare.security.user.PcUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class AppSecurityConfig {
+    private final PcUserDetailsService userDetailsService;
+    private final JwtAuthEntryPt authEntryPt;
+}
