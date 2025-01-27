8e0d1b33d5d7a9e958dce5824693cf7536b3f0ba
osparking <jbpark03@gmail.com>
2025-01-27 10:23:16 +0900

    authen' manager bean method

--- src/main/java/com/bumsoap/petcare/config/AppSecurityConfig.java
+++ src/main/java/com/bumsoap/petcare/config/AppSecurityConfig.java
@@ -6,6 +6,8 @@ import com.bumsoap.petcare.security.user.PcUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,4 +28,13 @@ public class AppSecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authConfig
+    ) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
 }
+
+
