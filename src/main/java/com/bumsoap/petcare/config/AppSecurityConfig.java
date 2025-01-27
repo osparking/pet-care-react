76e781cff27c36906f6a9c97085a3344421a8287
osparking <jbpark03@gmail.com>
2025-01-27 12:15:54 +0900

    security filter chain bean

--- src/main/java/com/bumsoap/petcare/config/AppSecurityConfig.java
+++ src/main/java/com/bumsoap/petcare/config/AppSecurityConfig.java
@@ -3,7 +3,6 @@ package com.bumsoap.petcare.config;
 import com.bumsoap.petcare.security.jwt.AuthTokenFilter;
 import com.bumsoap.petcare.security.jwt.JwtAuthEntryPt;
 import com.bumsoap.petcare.security.user.PcUserDetailsService;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +10,16 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -45,6 +51,23 @@ public class AppSecurityConfig {
         authenticationProvider.setUserDetailsService(userDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exception
+                        -> exception.authenticationEntryPoint(authEntryPt))
+                .sessionManagement(session
+                        -> session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth
+                        ->auth.requestMatchers("")
+                        .authenticated().anyRequest().permitAll());
+        http.authenticationProvider(authenticationProvider());
+        http.addFilterBefore(
+                authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 }
 
