eff1aedb7489536131239e61e5853b2929a3ea91
osparking <jbpark03@gmail.com>
2025-01-27 09:59:26 +0900

    creates authen' token filter bean

--- src/main/java/com/bumsoap/petcare/config/AppSecurityConfig.java
+++ src/main/java/com/bumsoap/petcare/config/AppSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.bumsoap.petcare.config;
 
+import com.bumsoap.petcare.security.jwt.AuthTokenFilter;
 import com.bumsoap.petcare.security.jwt.JwtAuthEntryPt;
 import com.bumsoap.petcare.security.user.PcUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -12,4 +14,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class AppSecurityConfig {
     private final PcUserDetailsService userDetailsService;
     private final JwtAuthEntryPt authEntryPt;
+
+    @Bean
+    public AuthTokenFilter authTokenFilter() {
+        return new AuthTokenFilter();
+    }
 }
