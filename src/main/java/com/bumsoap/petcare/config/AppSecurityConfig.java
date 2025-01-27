558e8597676e5e8b7d882d576a26d364068d57df
osparking <jbpark03@gmail.com>
2025-01-27 10:28:18 +0900

    authen' provider bean method

--- src/main/java/com/bumsoap/petcare/config/AppSecurityConfig.java
+++ src/main/java/com/bumsoap/petcare/config/AppSecurityConfig.java
@@ -3,10 +3,13 @@ package com.bumsoap.petcare.config;
 import com.bumsoap.petcare.security.jwt.AuthTokenFilter;
 import com.bumsoap.petcare.security.jwt.JwtAuthEntryPt;
 import com.bumsoap.petcare.security.user.PcUserDetailsService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,6 +37,14 @@ public class AppSecurityConfig {
             AuthenticationConfiguration authConfig
     ) throws Exception {
         return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        var authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 }
 
