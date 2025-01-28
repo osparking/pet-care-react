9dcaa789b2eda30d243ef60b0a2ee4c812d6803d
osparking <jbpark03@gmail.com>
2025-01-28 22:45:45 +0900

    prepend with annotation on the LoginRequest members

--- src/main/java/com/bumsoap/petcare/request/LoginRequest.java
+++ src/main/java/com/bumsoap/petcare/request/LoginRequest.java
@@ -1,6 +1,12 @@
 package com.bumsoap.petcare.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+    @NotBlank
     private String email;
+    @NotBlank
     private String password;
 }
