bdc705b522060f2448dc76c36cd9b94c40bf3637
osparking <jbpark03@gmail.com>
2025-01-29 18:49:27 +0900

    auth endpoint handler - upper part

--- src/main/java/com/bumsoap/petcare/controller/ControllerAuth.java
+++ src/main/java/com/bumsoap/petcare/controller/ControllerAuth.java
@@ -3,14 +3,29 @@ package com.bumsoap.petcare.controller;
 import com.bumsoap.petcare.request.LoginRequest;
 import com.bumsoap.petcare.response.ApiResponse;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class ControllerAuth {
+    private final AuthenticationManager authenticationManager;
     public ResponseEntity<ApiResponse> login (@Valid LoginRequest request) {
-
+        try {
+            var authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getEmail(), request.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(
+                    authentication);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
