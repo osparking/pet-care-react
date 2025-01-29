76ebe4eb9e99c53ce70b143a3eec5883f5076487
osparking <jbpark03@gmail.com>
2025-01-29 19:50:36 +0900

    add missing postmapping

--- src/main/java/com/bumsoap/petcare/controller/ControllerAuth.java
+++ src/main/java/com/bumsoap/petcare/controller/ControllerAuth.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
@@ -27,6 +28,7 @@ public class ControllerAuth {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
 
+    @PostMapping("/login")
     public ResponseEntity<ApiResponse> login (@Valid LoginRequest request) {
         try {
             var authentication = authenticationManager.authenticate(
