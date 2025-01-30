28510d96f5a1f501fc0afe68db029dbc7d75256a
osparking <jbpark03@gmail.com>
2025-01-30 17:44:02 +0900

    add 5 anno to class and id member

--- src/main/java/com/bumsoap/petcare/model/VerifToken.java
+++ src/main/java/com/bumsoap/petcare/model/VerifToken.java
@@ -1,11 +1,17 @@
 package com.bumsoap.petcare.model;
 
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+@Entity
+@Data
+@NoArgsConstructor
 public class VerifToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String token;
     private Date expireDate;
