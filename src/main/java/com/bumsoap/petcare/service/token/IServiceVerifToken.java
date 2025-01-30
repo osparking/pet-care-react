f6f98a04fa4701b854b06f549bc109c0c4c50439
osparking <jbpark03@gmail.com>
2025-01-30 21:06:24 +0900

    add three additional methods to ISVToken

--- src/main/java/com/bumsoap/petcare/service/token/IServiceVerifToken.java
+++ src/main/java/com/bumsoap/petcare/service/token/IServiceVerifToken.java
@@ -1,9 +1,15 @@
 package com.bumsoap.petcare.service.token;
 
 import com.bumsoap.petcare.model.User;
+import com.bumsoap.petcare.model.VerifToken;
+
+import java.util.Optional;
 
 public interface IServiceVerifToken {
     String validateToken(String token);
     void saveUserVerifToken(String token, User user);
+    VerifToken makeNewToken(String oldToken);
+    Optional<VerifToken> findByToken(String token);
+    void deleteTokenById(Long id);
 
 }
