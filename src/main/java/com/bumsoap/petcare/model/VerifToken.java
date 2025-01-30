d93aed00b14c098b8b0216d431a50c9523eea3a8
osparking <jbpark03@gmail.com>
2025-01-30 17:42:10 +0900

    add user member with 2 anno'

--- src/main/java/com/bumsoap/petcare/model/VerifToken.java
+++ src/main/java/com/bumsoap/petcare/model/VerifToken.java
@@ -1,10 +1,17 @@
 package com.bumsoap.petcare.model;
 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 import java.util.Date;
 
 public class VerifToken {
     private Long id;
     private String token;
     private Date expireDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
 }
