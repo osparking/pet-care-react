12d740d7368d9025425e3031f1548557d231211a
osparking <jbpark03@gmail.com>
2025-01-31 14:14:33 +0900

    members and endpoint handler

--- src/main/java/com/bumsoap/petcare/controller/ControllerTokenVerif.java
+++ src/main/java/com/bumsoap/petcare/controller/ControllerTokenVerif.java
@@ -1,7 +1,12 @@
 package com.bumsoap.petcare.controller;
 
+import com.bumsoap.petcare.repository.RepositoryUser;
+import com.bumsoap.petcare.response.ApiResponse;
+import com.bumsoap.petcare.service.token.IServiceVerifToken;
+import com.bumsoap.petcare.utils.FeedbackMessage;
 import com.bumsoap.petcare.utils.UrlMapping;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping(UrlMapping.VERIFY_TOKEN)
 public class ControllerTokenVerif {
+    private final IServiceVerifToken serviceVerifToken;
+    private final RepositoryUser repositoryUser;
+
+    public ResponseEntity<ApiResponse> validateToken(String token) {
+        String userId = serviceVerifToken.validateToken(token);
+        return null;
+    }
 }
