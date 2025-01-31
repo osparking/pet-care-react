f4184902ec8d0ca9561ad4cdf0539ea48834967e
osparking <jbpark03@gmail.com>
2025-01-31 11:34:55 +0900

    custom method to fetch token from DB

--- src/main/java/com/bumsoap/petcare/repository/RepositoryVerifToken.java
+++ src/main/java/com/bumsoap/petcare/repository/RepositoryVerifToken.java
@@ -3,5 +3,8 @@ package com.bumsoap.petcare.repository;
 import com.bumsoap.petcare.model.VerifToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RepositoryVerifToken extends JpaRepository<VerifToken, Long> {
+    Optional<VerifToken> findByToken(String token);
 }
