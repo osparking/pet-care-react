e538feb8163d4888bb5216ba13bc3e278b7fa9a1
osparking <jbpark03@gmail.com>
2025-02-01 15:18:35 +0900

    defines frontend base url

--- src/main/java/com/bumsoap/petcare/event/listener/NotiEventListener.java
+++ src/main/java/com/bumsoap/petcare/event/listener/NotiEventListener.java
@@ -3,13 +3,18 @@ package com.bumsoap.petcare.event.listener;
 import com.bumsoap.petcare.email.EmailComponent;
 import com.bumsoap.petcare.service.token.IServiceVerifToken;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
 
+@Component
 @RequiredArgsConstructor
 public class NotiEventListener implements ApplicationListener<ApplicationEvent> {
     private final EmailComponent emailComponent;
     private final IServiceVerifToken serviceToken;
+    @Value("${frontend.base.url}")
+    private String frontendBaseUrl;
 
     @Override
     public void onApplicationEvent(ApplicationEvent event) {
