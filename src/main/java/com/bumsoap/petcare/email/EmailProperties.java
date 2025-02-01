8f5fe580405b2058aa0c0ef38367511f5341ce1a
osparking <jbpark03@gmail.com>
2025-02-01 10:31:06 +0900

    email properties with password from google

--- src/main/java/com/bumsoap/petcare/email/EmailProperties.java
+++ src/main/java/com/bumsoap/petcare/email/EmailProperties.java
@@ -1,4 +1,15 @@
 package com.bumsoap.petcare.email;
 
+import lombok.Data;
+
+@Data
 public class EmailProperties {
+    public static final String DEFAULT_HOST = "smtp.gmail.com";
+    public static final int DEFAULT_PORT = 587;
+    public static final String DEFAULT_SENDER = "jbpark03@gmail.com";
+    public static final String DEFAULT_USERNAME = "jbpark03@gmail.com";
+    public static final String DEFAULT_PASSWORD
+            = System.getenv("JB_PET_CARE_PWD");
+    public static final boolean DEFAULT_AUTH = true;
+    public static final boolean DEFAULT_STARTTLS = true;
 }
