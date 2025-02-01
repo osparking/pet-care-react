cdc8bd18b9885d0fdbb4053cb8873d5fab8ee4bf
osparking <jbpark03@gmail.com>
2025-02-01 10:57:19 +0900

    compose upper half of mail itself

--- src/main/java/com/bumsoap/petcare/email/EmailComponent.java
+++ src/main/java/com/bumsoap/petcare/email/EmailComponent.java
@@ -1,13 +1,22 @@
 package com.bumsoap.petcare.email;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
 
 @Component
 public class EmailComponent {
     private JavaMailSender mailSender;
     public void sendEmail(String to, String subject, String senderName,
-                          String mailContent) {
+                          String mailContent) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        var messageHelper = new MimeMessageHelper(message);
+        messageHelper.setFrom(EmailProperties.DEFAULT_USERNAME, senderName);
+        messageHelper.setTo(to);
 
     }
 }
