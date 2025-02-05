826c7315b7622769d503202395dea22217deedd5
osparking <jbpark03@gmail.com>
2025-02-05 13:58:35 +0900

    check if string is one of validation feedbacks

--- src/main/java/com/bumsoap/petcare/utils/SystemUtils.java
+++ src/main/java/com/bumsoap/petcare/utils/SystemUtils.java
@@ -1,7 +1,9 @@
 package com.bumsoap.petcare.utils;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class SystemUtils {
     private static final int EXPIRE_MIN = 10;
@@ -11,5 +13,20 @@ public class SystemUtils {
         calendar.setTimeInMillis(new Date().getTime());
         calendar.add(Calendar.MINUTE, EXPIRE_MIN);
         return new Date(calendar.getTime().getTime());
+    }
+
+    public static boolean isValidateFeedback(String token) {
+        List<String> tokenValidationFeedback = new ArrayList<>();
+
+        tokenValidationFeedback.add(FeedbackMessage.NOT_FOUND_VERIF_TOKEN);
+        tokenValidationFeedback.add(FeedbackMessage.VERIFIED_TOKEN);
+        tokenValidationFeedback.add(FeedbackMessage.TOKEN_EXPIRED);
+        tokenValidationFeedback.add(FeedbackMessage.TOKEN_VALIDATED);
+
+        if (!tokenValidationFeedback.contains(token)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
