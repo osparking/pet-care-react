92bcbb94eed7da15e39fc944a62617fba01030c1
osparking <jbpark03@gmail.com>
2025-02-06 20:35:54 +0900

    add three get/find interface methods

--- src/main/java/com/bumsoap/petcare/service/role/ServiceRoleI.java
+++ src/main/java/com/bumsoap/petcare/service/role/ServiceRoleI.java
@@ -1,5 +1,13 @@
 package com.bumsoap.petcare.service.role;
 
+import com.bumsoap.petcare.model.Role;
+
+import java.util.List;
+
 public interface ServiceRoleI {
+    List<Role> getRoles();
+    Role findByName(String name);
+    Role findById(Long id);
+
 
 }
