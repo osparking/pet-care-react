026f73357023e5b96fdc8ac168c3843bb5eaaea2
osparking <jbpark03@gmail.com>
2025-02-06 20:57:10 +0900

    method heading, annot', and reposi member

--- src/main/java/com/bumsoap/petcare/service/role/ServiceRole.java
+++ src/main/java/com/bumsoap/petcare/service/role/ServiceRole.java
@@ -1,4 +1,40 @@
 package com.bumsoap.petcare.service.role;
 
+import com.bumsoap.petcare.model.Role;
+import com.bumsoap.petcare.repository.RepositoryRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
 public class ServiceRole implements ServiceRoleI {
+    private final RepositoryRole roleRepository;
+
+    @Override
+    public List<Role> getRoles() {
+        return List.of();
+    }
+
+    @Override
+    public Role findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Role findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void saveRole(Role role) {
+
+    }
+
+    @Override
+    public Collection<Role> setUserRoles(List<String> roleNames) {
+        return List.of();
+    }
 }
