6b965898d43a54ee12dff39eefc78434c2521364
osparking <jbpark03@gmail.com>
2025-02-06 21:02:53 +0900

    method finds a role by role name

--- src/main/java/com/bumsoap/petcare/repository/RepositoryRole.java
+++ src/main/java/com/bumsoap/petcare/repository/RepositoryRole.java
@@ -4,4 +4,5 @@ import com.bumsoap.petcare.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RepositoryRole extends JpaRepository<Role, Long> {
+    Role findByName(String name);
 }
