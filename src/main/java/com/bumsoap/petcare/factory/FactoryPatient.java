d3828f61cc978ec88d9d730b246b469c36ccdfc6
osparking <jbpark03@gmail.com>
2025-02-07 19:55:11 +0900

    patient factory creator sets role set

--- src/main/java/com/bumsoap/petcare/factory/FactoryPatient.java
+++ src/main/java/com/bumsoap/petcare/factory/FactoryPatient.java
@@ -4,6 +4,7 @@ import com.bumsoap.petcare.model.Patient;
 import com.bumsoap.petcare.model.User;
 import com.bumsoap.petcare.repository.RepositoryPatient;
 import com.bumsoap.petcare.request.RegistrationRequest;
+import com.bumsoap.petcare.service.role.ServiceRoleI;
 import com.bumsoap.petcare.service.user.UserAttributesMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,11 @@ import org.springframework.stereotype.Service;
 public class FactoryPatient {
     private final RepositoryPatient repositoryPatient;
     private final UserAttributesMapper userAttributesMapper;
+    private final ServiceRoleI roleService;
 
     public Patient createPatient(RegistrationRequest request) {
         Patient patient = new Patient();
+        patient.setRoles(roleService.getRoleSet("PATIENT"));
         userAttributesMapper.setCommonAttributes(request, patient);
         return repositoryPatient.save(patient);
     }
