2d1ff3a656adfa59dd2cefd76caf3847996029c7
osparking <jbpark03@gmail.com>
2025-02-07 19:58:28 +0900

    veteri' factory creator sets role set

--- src/main/java/com/bumsoap/petcare/factory/FactoryVet.java
+++ src/main/java/com/bumsoap/petcare/factory/FactoryVet.java
@@ -4,6 +4,7 @@ import com.bumsoap.petcare.model.User;
 import com.bumsoap.petcare.model.Veterinarian;
 import com.bumsoap.petcare.repository.RepositoryVet;
 import com.bumsoap.petcare.request.RegistrationRequest;
+import com.bumsoap.petcare.service.role.ServiceRoleI;
 import com.bumsoap.petcare.service.user.UserAttributesMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,11 @@ import org.springframework.stereotype.Service;
 public class FactoryVet {
     private final RepositoryVet veterinarianRepository;
     private final UserAttributesMapper userAttributesMapper;
+    private final ServiceRoleI roleService;
 
     public Veterinarian createVeterinarian(RegistrationRequest request) {
         Veterinarian veterinarian = new Veterinarian();
+        veterinarian.setRoles(roleService.getRoleSet("VET"));
         userAttributesMapper.setCommonAttributes(request, veterinarian);
         veterinarian.setSpecialization(request.getSpecialization());
         return veterinarianRepository.save(veterinarian);
