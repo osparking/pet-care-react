3b98cb555e874066e96232f875bb379c1a5f2235
osparking <jbpark03@gmail.com>
2025-02-11 11:08:10 +0900

    uncomment the URL line to make it require authen

--- src/main/java/com/bumsoap/petcare/config/AppSecurityConfig.java
+++ src/main/java/com/bumsoap/petcare/config/AppSecurityConfig.java
@@ -48,7 +48,7 @@ public class AppSecurityConfig {
     }
 
     private static  String[] URLS = {
-//            "/api/v1/appointments/create",
+            "/api/v1/appointments/create",
             "/api/v1/reviews/**"
     };
 
