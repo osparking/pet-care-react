ba3f3d109f6bcbd1c8111dc0602e86c8fa9f9905
osparking <jbpark03@gmail.com>
2025-02-11 11:00:07 +0900

    add missing annot' for toke filter bean

--- src/main/java/com/bumsoap/petcare/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/bumsoap/petcare/security/jwt/AuthTokenFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -21,7 +22,9 @@ import java.io.IOException;
 @NoArgsConstructor
 @Component
 public class AuthTokenFilter extends OncePerRequestFilter {
+    @Autowired
     private JwtUtil jwtUtil;
+    @Autowired
     PcUserDetailsService userDetailsService;
 
     @Override
