098c3bc8dbe450c662de3ab935233bc48949831c
osparking <jbpark03@gmail.com>
2025-02-13 14:31:04 +0900

    changes status of all appointments

--- src/main/java/com/bumsoap/petcare/schedule/AppoStatusChanger.java
+++ src/main/java/com/bumsoap/petcare/schedule/AppoStatusChanger.java
@@ -4,10 +4,17 @@ import com.bumsoap.petcare.service.appointment.IServiceAppointment;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @AllArgsConstructor
 public class AppoStatusChanger {
     private final IServiceAppointment appointService;
 
-
+    public void automateAppointStatusChange() {
+        List<Long> appointIds = appointService.appointIds();
+        for (Long appointId : appointIds) {
+            appointService.setAppointStatus(appointId);
+        }
+    }
 }
