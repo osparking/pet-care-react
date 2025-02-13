09ea136d395b415e234594b8e598f690980e52ea
osparking <jbpark03@gmail.com>
2025-02-13 17:49:02 +0900

    specifies how often the automate method runs

--- src/main/java/com/bumsoap/petcare/schedule/AppoStatusChanger.java
+++ src/main/java/com/bumsoap/petcare/schedule/AppoStatusChanger.java
@@ -2,6 +2,7 @@ package com.bumsoap.petcare.schedule;
 
 import com.bumsoap.petcare.service.appointment.IServiceAppointment;
 import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,6 +12,15 @@ import java.util.List;
 public class AppoStatusChanger {
     private final IServiceAppointment appointService;
 
+    /** 실행 빈도 지정 항목 설명
+     * 0 : 해당 분, 최초(0초)
+     * 0/2 : 해당 시각의 최초 부터, 2분 간격
+     * '*' : 해당 일의 매 시간(=시각)
+     * 1/1 : 해당 월의 매 일
+     * '*' : 매 월
+     * '?' : 각 주의 매 요일
+     */
+    @Scheduled(cron = "0 0/2 * 1/1 * ?")
     public void automateAppointStatusChange() {
         List<Long> appointIds = appointService.appointIds();
         for (Long appointId : appointIds) {
