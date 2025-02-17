ef84f256922dcc6c6d2b33d95a96dc56d0480574
osparking <jbpark03@gmail.com>
2025-02-17 18:25:50 +0900

    reposi' method - delete expired verif' token records

--- src/main/java/com/bumsoap/petcare/repository/RepositoryVerifToken.java
+++ src/main/java/com/bumsoap/petcare/repository/RepositoryVerifToken.java
@@ -2,9 +2,15 @@ package com.bumsoap.petcare.repository;
 
 import com.bumsoap.petcare.model.VerifToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface RepositoryVerifToken extends JpaRepository<VerifToken, Long> {
     Optional<VerifToken> findByToken(String token);
+
+    @Query(value =
+            "DELETE FROM verif_token WHERE id != :vtId AND user_id = :userId")
+    void deleteVeToken(@Param("vtId") Long vtId, @Param("userId") Long userId);
 }
