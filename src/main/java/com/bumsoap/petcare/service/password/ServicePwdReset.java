9f4665c19f4f0c243e8d7c3bdd146bbfc4f81e8a
osparking <jbpark03@gmail.com>
2025-02-20 17:30:07 +0900

    four more members for pwd reset service impl'

--- src/main/java/com/bumsoap/petcare/service/password/ServicePwdReset.java
+++ src/main/java/com/bumsoap/petcare/service/password/ServicePwdReset.java
@@ -3,7 +3,9 @@ package com.bumsoap.petcare.service.password;
 import com.bumsoap.petcare.model.User;
 import com.bumsoap.petcare.repository.RepositoryUser;
 import com.bumsoap.petcare.repository.RepositoryVerifToken;
+import com.bumsoap.petcare.service.token.ServiceVerifToken;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,10 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ServicePwdReset implements  ServicePwdResetI{
     private final RepositoryVerifToken verifTokenRepository;
+    private final RepositoryUser userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final ApplicationEventPublisher eventPublisher;
+    private final ServiceVerifToken verifTokenService;
 
     @Override
     public Optional<User> findUserByResetToken(String token) {
