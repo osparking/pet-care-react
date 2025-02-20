3bfc6e0c8d48d84631981b2bc3495825067396f9
osparking <jbpark03@gmail.com>
2025-02-20 15:54:12 +0900

    three method of password reset service interface

--- src/main/java/com/bumsoap/petcare/service/password/ServicePwdReset.java
+++ src/main/java/com/bumsoap/petcare/service/password/ServicePwdReset.java
@@ -1,4 +1,8 @@
 package com.bumsoap.petcare.service.password;
 
+import com.bumsoap.petcare.model.User;
+
+import java.util.Optional;
+
 public class ServicePwdReset implements  ServicePwdResetI{
 }
--- src/main/java/com/bumsoap/petcare/service/password/ServicePwdResetI.java
+++ src/main/java/com/bumsoap/petcare/service/password/ServicePwdResetI.java
@@ -1,4 +1,10 @@
 package com.bumsoap.petcare.service.password;
 
+import com.bumsoap.petcare.model.User;
+import java.util.Optional;
+
 public interface ServicePwdResetI {
+    Optional<User> findUserByResetToken(String token);
+    void passwordResetRequest(String email);
+    String resetPassword(String password, User user);
 }
