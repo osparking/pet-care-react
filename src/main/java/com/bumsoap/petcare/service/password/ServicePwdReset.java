0708405d77c65dc3b255753f9bfe822525b8077b
osparking <jbpark03@gmail.com>
2025-02-20 16:00:36 +0900

    pwd reset service class impl inter'

--- src/main/java/com/bumsoap/petcare/service/password/ServicePwdReset.java
+++ src/main/java/com/bumsoap/petcare/service/password/ServicePwdReset.java
@@ -1,8 +1,31 @@
 package com.bumsoap.petcare.service.password;
 
 import com.bumsoap.petcare.model.User;
+import com.bumsoap.petcare.repository.RepositoryUser;
+import com.bumsoap.petcare.repository.RepositoryVerifToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
+@RequiredArgsConstructor
 public class ServicePwdReset implements  ServicePwdResetI{
+    private final RepositoryVerifToken verifTokenRepository;
+
+    @Override
+    public Optional<User> findUserByResetToken(String token) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void passwordResetRequest(String email) {
+
+    }
+
+    @Override
+    public String resetPassword(String password, User user) {
+        return "";
+    }
 }
