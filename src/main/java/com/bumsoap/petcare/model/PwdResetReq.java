75309659720349babe720e7d673dfb27714209a8
osparking <jbpark03@gmail.com>
2025-02-20 14:35:42 +0900

    password reset request model/entity class

---
+++ src/main/java/com/bumsoap/petcare/model/PwdResetReq.java
@@ -0,0 +1,11 @@
+package com.bumsoap.petcare.model;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class PwdResetReq {
+}
