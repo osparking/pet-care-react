eb13026de30c560c014210c31544453d08c5d965
osparking <jbpark03@gmail.com>
2025-02-20 14:42:51 +0900

    adds 4 member fields to request class

--- src/main/java/com/bumsoap/petcare/model/PwdResetReq.java
+++ src/main/java/com/bumsoap/petcare/model/PwdResetReq.java
@@ -1,11 +1,21 @@
 package com.bumsoap.petcare.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class PwdResetReq {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    private Date expiresOnTm;
+    private User user;
 }
